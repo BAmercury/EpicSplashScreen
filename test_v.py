be86ad9876508fbe940df7d56dc70b849dadd752
BrianAmin <tuf49787@temple.edu>
2018-02-02 10:33:40 -0500

    Got random chars printing to the splash screen. Will add in now the ability to load preloaded sentences from file

--- test_v.py
+++ test_v.py
@@ -1,11 +1,10 @@
 
 from time import sleep
-import Tkinter as tk
+from Tkinter import *
 import threading
 import random
 
 # from www.sunjay-varma.com
-
 class SplashScreen(Frame):
     def __init__(self, master=None, width=0.4, height=0.3, useFactor=True):
         Frame.__init__(self, master)
@@ -24,24 +23,38 @@ class SplashScreen(Frame):
         self.master.overrideredirect(True)
         self.lift()
 
-root = tk.Tk()
+root = Tk()
 sp  = SplashScreen(root)
-label = tk.Label(sp, width=45)
+sp.config(bg="#3366ff")
+label = Label(sp, width=45)
 label.pack(side=TOP, expand=YES)
 label.config(bg="#3366ff", justify=CENTER, font=("calibri", 29))
 Button(sp, text="Press this button to kill the program", bg='red', command=root.destroy).pack(side=BOTTOM, fill=X)
 
-def strings_from_file():
+foo = []
 
+def strings_from_file():
+    global foo
+    foo = ['a', 'b', 'c', 'd', 'e']
 
 
 def update_test():
-    
+    strings_from_file()
+
+    while True:
+        #print('here')
+        sleep(0.5)
+        string = random.choice(foo)
+        label.config(text=string)
+        label.pack(side=TOP, expand=YES)
+        label.config(bg="#3366ff", justify=CENTER, font=("calibri", 29))
 
 
 def main():
 
     # start a thread that will update text
+    string_thread = threading.Thread(target=lambda:update_test())
+    string_thread.start()
 
     root.mainloop()
 
